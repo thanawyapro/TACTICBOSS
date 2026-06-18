61abc7f0ee813d0742af57681073cb55e56f7062
thanawyapro <hfarahat51@gmail.com>
2026-06-18 12:27:58 +0200

    Deploy

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,29 +1,11 @@
-// Tactic Boss V132.3 SW KILLER
-// No fetch handler. This file only removes old broken service workers and caches.
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
+// Tactic Boss V132.4: Service Worker killer - unregister and clear caches
+self.addEventListener('install', event => { self.skipWaiting(); });
+self.addEventListener('activate', event => {
   event.waitUntil((async () => {
-    try {
-      const keys = await caches.keys();
-      await Promise.all(keys.map((key) => caches.delete(key)));
-    } catch (_) {}
-
-    try {
-      await self.clients.claim();
-    } catch (_) {}
-
-    try {
-      await self.registration.unregister();
-    } catch (_) {}
-
-    try {
-      const clientsList = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
-      for (const client of clientsList) {
-        try { client.postMessage({ type: 'TB_SW_KILLED', version: '132.3' }); } catch (_) {}
-      }
-    } catch (_) {}
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+    await self.registration.unregister();
+    const clientsList = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    for (const client of clientsList) client.navigate(client.url);
   })());
 });
